fc897a8e16414b49e33a75d06269492aa4d7ab59
Juan Bermudez Roldan <>
2018-02-25 16:45:50 +0100

    It works

--- src/main/java/com/juan/test/config/GlobalConfig.java
+++ src/main/java/com/juan/test/config/GlobalConfig.java
@@ -1,16 +1,13 @@
 package com.juan.test.config;
 
 import com.juan.test.filter.CustomSpringCrnkFilter;
-import io.crnk.core.boot.CrnkBoot;
 import io.crnk.spring.boot.CrnkSpringBootProperties;
 import io.crnk.spring.internal.SpringServiceDiscovery;
-import io.crnk.spring.mvc.SpringMvcModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan({"com.juan.test.resources", "com.juan.test.config"})
